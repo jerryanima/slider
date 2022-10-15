d9b34442af450df00be61796bdf77a05cffa58fd
maria.lapina <mia_lapina@list.ru>
2022-10-15 23:22:57 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,16 @@
+let images = [{
+    url: "https://images.wallpaperscraft.ru/image/single/devushka_sumerki_oblaka_156445_1280x720.jpg"
+  }, {
+    url: "https://images.wallpaperscraft.ru/image/single/devushka_trava_gorod_213102_1280x720.jpg"
+  }, {
+    url: "https://images.wallpaperscraft.ru/image/single/siluet_noch_zvezdnoe_nebo_137292_1280x720.jpg"
+  }, {
+    url: "https://images.wallpaperscraft.ru/image/single/devushka_pantsir_zajts_167320_1280x720.jpg"
+  }, {
+    url: "https://images.wallpaperscraft.ru/image/single/devushka_kotenok_tsvetok_141058_1280x720.jpg"
+}];
+
+
+function iniSlider() {
+    
+}
